4141ff2c76dfc8719bfec5cef1e3f7f67ba74af0
Donqueso <kg.v.ekeren@gmail.com>
2018-12-19 18:58:47 +0100

    soln day16 p 2

--- 2018/day16.py
+++ 2018/day16.py
@@ -24,33 +24,48 @@ operators = {
 rgx = re.compile(r'[^\d]')
 
 
+def num_funcs_applicable(testops, a, b, c, before, after):
+    func_cnt = 0
+    last_op = None
+    for opname, operator in testops.items():
+        tmp = before.copy()
+        tmp[c] = operator(a, b, before)
+        if tmp == after:
+            last_op = opname
+            func_cnt += 1
+    return func_cnt, last_op
+
+
 if __name__ == '__main__':
     data = open('input16p1').read().splitlines()
     cnt = 0
-    opcodes = defaultdict(list)
+    samples_per_opcode = defaultdict(list)   # storage for number resolution p2
     for i in range(0, len(data), 4):
         before = [int(x) for x in re.sub(rgx, ' ', data[i]).strip().split()]
         opcode, a, b, c = [int(x) for x in re.sub(rgx, ' ', data[i + 1]).strip().split()]
         after = [int(x) for x in re.sub(rgx, ' ', data[i + 2]).strip().split()]
-        func_cnt = 0
-        for opname, operator in operators.items():
-            tmp = before.copy()
-            tmp[c] = operator(a, b, before)
-            if tmp == after:
-                opcodes[opcode].append(opname)
-                func_cnt += 1
+        func_cnt, _ = num_funcs_applicable(operators, a, b, c, before, after)
         if func_cnt >= 3:
             cnt += 1
+        samples_per_opcode[opcode].append((a, b, c, before, after))
     print("Part 1: " + str(cnt))
-    print(opcodes)
-    opcode2opname = {opcode: Counter(opnames).most_common()[0][0] for opcode, opnames in opcodes.items()}
+
+    # Figure out number-opcode mapping by knockout
+    opcode2opname = {}
+
+    knockout = operators.copy()
+    while len(opcode2opname) < len(operators):
+        for opcode, samples in samples_per_opcode.items():
+            func_cnt, opname = num_funcs_applicable(knockout, *samples[0])
+            if func_cnt == 1:
+                opcode2opname[opcode] = opname
+                samples_per_opcode.pop(opcode)
+                knockout.pop(opname)
+                break
 
     program = open('input16p2').read().splitlines()
     registers = [0, 0, 0, 0]
     for instruction in program:
-        print("State " + str(registers))
         opcode, a, b, c = [int(x) for x in instruction.strip().split()]
-        print("Executing {} a: {} b: {} c: {}".format(opcode2opname[opcode], a, b, c))
         registers[c] = operators[opcode2opname[opcode]](a, b, registers)
-
     print("Part 2: " + str(registers[0]))
