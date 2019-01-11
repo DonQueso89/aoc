b938a43e3e83bef7a4fce323e6dc06fc426fbb46
Donqueso <kg.v.ekeren@gmail.com>
2019-01-11 20:47:29 +0100

    Day 5 2015 in Ruby

--- 2015/day5.rb
+++ 2015/day5.rb
@@ -1,23 +1,32 @@
 #!/usr/bin/env ruby
 require('digest')
+require('set')
 
 input = File.read(ARGV[0])
 
-cnt = 0
-rgx = /^00000/
+ZE_LATIN_ALPHABETTHH = "abcdefghijklmnopqrstuvwxyz"
+doppios_rgx = Regexp.new ZE_LATIN_ALPHABETTHH.chars.map { |e| e + e } .join "|"
 
-while true
-    h = Digest::MD5.hexdigest("#{input}#{cnt}")
-    break if rgx.match(h) != nil
-    cnt += 1
-end
-puts "Part 1: #{cnt}"
+is_nice = lambda { |s|
+    /ab|cd|pq|xy/.match(s) == nil &&
+    s.scan(/[aeiou]/).size > 2 &&
+    doppios_rgx.match(s) != nil ? 1 : 0
+}
 
-rgx = /^000000/
+puts "Part 1: #{input.split().map { |s| is_nice.call(s) } .sum}"
 
-while true
-    h = Digest::MD5.hexdigest("#{input}#{cnt}")
-    break if rgx.match(h) != nil
-    cnt += 1
-end
-puts "Part 2: #{cnt}"
+# scan looks for non-overlapping occurrences
+palindromes_rgx = Regexp.new ZE_LATIN_ALPHABETTHH.chars.map { 
+    |outer| ZE_LATIN_ALPHABETTHH.tr(outer, "").chars.map { 
+        |inner| outer + inner + outer } .join "|" } .join("|")
+
+doppios_rgx = Regexp.new ZE_LATIN_ALPHABETTHH.chars.map { 
+    |left| ZE_LATIN_ALPHABETTHH.chars.map { 
+        |right| (left + right) + ".*" + (left + right) } .join "|" } .join("|")
+
+is_nice = lambda { |s|
+    palindromes_rgx.match(s) != nil &&
+        doppios_rgx.match(s) != nil ? 1 : 0
+}
+
+puts "Part 2: #{input.split().map { |s| is_nice.call(s) } .sum }"
