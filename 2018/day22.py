845c9521f5240dc4d9d88c8c82bf5aa3328f0a55
Donqueso <kg.v.ekeren@gmail.com>
2019-12-09 08:44:04 +0100

    fix target error

--- 2018/day22.py
+++ 2018/day22.py
@@ -1,6 +1,7 @@
 import time
 import argparse
 import heapq
+import networkx 
 from collections import defaultdict
 from functools import partial
 
@@ -8,15 +9,6 @@ parser = argparse.ArgumentParser(description='Solve day 22')
 parser.add_argument('depth', type=int, help='depth of cave')
 parser.add_argument('targetX', type=int, help='target x coordinate')
 parser.add_argument('targetY', type=int, help='target y coordinate')
-
-
-'''
-start with the torch equipped at (0, 0)
-In rocky regions, you can use the climbing gear or the torch. You cannot use neither (you'll likely slip and fall).
-In wet regions, you can use the climbing gear or neither tool. You cannot use the torch (if it gets wet, you won't have a light source).
-In narrow regions, you can use the torch or neither tool. You cannot use the climbing gear (it's too bulky to fit).
-At the end, you need the torch
-'''
 
 
 ROCK = 0
@@ -50,28 +42,6 @@ tm = {
 }
 
 
-def neighbours(cave_map, x, y, current_tool, visited):
-    """ 
-    Treat each possible (x, y, tool) combo as a separate vertex
-    """
-    candidates = set([])
-    for offsetX, offsetY in [
-        (0, 1),
-        (1, 0),
-        (-1, 0),
-        (0, -1)
-    ]:
-        sx, sy = x + offsetX, y + offsetY
-        if (sx, sy) not in cave_map:
-            continue
-        if current_tool in tools_for[cave_map[(sx, sy)]]:
-            candidates.add((sx, sy, current_tool))
-    for tool in tools_for[cave_map[(x, y)]] - set([current_tool]):
-        candidates.add((x, y, tool))
-
-    return candidates - visited
-
-
 def _get_erosion_level(erosion_levels, depth, x, y):
     if y == 0:
         return (x * 16807 + depth) % 20183
@@ -91,6 +61,7 @@ def display(cave_map, tx, ty, cx, cy, tool):
                 result += " T"
             else:
                 result += (" " + str(dm[cave_map[(x, y)]]))
+    print(result)
 
 
 def display_queue(pq, cave_map, tx, ty, tt, sx, sy, st, visited):
@@ -111,93 +82,111 @@ def display_queue(pq, cave_map, tx, ty, tt, sx, sy, st, visited):
             print(line)
 
 
-def generate_cave_map(depth, tx, ty):
+def generate_cave_map(depth, tx, ty, max_x, max_y):
     erosion_levels = defaultdict(int)
-    erosion_levels[(0, 0)] = 0 + depth
+    erosion_levels[(0, 0)] = depth % 20183
 
     get_erosion_level = partial(_get_erosion_level, erosion_levels, depth)
 
-    # Calculate west and north borders first to avoid the overhead of checking
-    # whether the coordinate is the cavemouth on every iteration
     # North border
-    for y in range(0, 1):
-        for x in range(1, tx + 1):
-            erosion_levels[(x, y)] = get_erosion_level(x, y)
+    for x in range(1, max_x):
+        erosion_levels[(x, 0)] = get_erosion_level(x, 0)
 
     # West border
-    for y in range(1, ty + 1):
-        for x in range(0, 1):
+    for y in range(1, max_y):
+        erosion_levels[(0, y)] = get_erosion_level(0, y)
+
+    for y in range(1, max_y):
+        for x in range(1, max_x):
             erosion_levels[(x, y)] = get_erosion_level(x, y)
 
-    for y in range(1, ty + 1):
-        for x in range(1, tx + 1):
-            erosion_levels[(x, y)] = get_erosion_level(x, y)
-
-    erosion_levels[(tx, ty)] = 0 + depth
+    erosion_levels[(tx, ty)] = depth % 20183
     cave_map = {coordinate: el % 3 for coordinate, el in erosion_levels.items()}
     return cave_map
 
 
+def get_graph(grid):
+    G = networkx.Graph()
+    for (x, y), _type in grid.items():
+        for tool in tools_for[_type]:
+            G.add_node((x, y, tool))
+
+            for nx, ny in [
+                (x - 1, y),
+                (x + 1, y),
+                (x, y - 1),
+                (x, y + 1),
+            ]:
+                if (nx, ny) in grid:
+                    for ntool in tools_for[grid[(nx, ny)]]:
+                        if ntool == tool:
+                            G.add_edge(
+                                (x, y, tool),
+                                (nx, ny, ntool),
+                                weight=1
+                            )
+
+            for ntool in tools_for[_type]:
+                if ntool != tool:
+                    G.add_edge((x, y, tool), (x, y, ntool), weight=7)
+    return G
+
+
+
+
 def solve(depth, tx, ty):
-    return sum(list(generate_cave_map(depth, tx, ty).values()))
+    grid = generate_cave_map(depth, tx, ty, depth, depth)
+    #display(grid, tx, ty, 0, 0, 1)
 
+    G = get_graph(grid)
+    print("NetworkX says: ",
+            networkx.shortest_path_length(G, (0, 0, TORCH), (tx, ty, TORCH), weight='weight'))
 
-def _distance(c1, c2):
-    distance = 0
-    x1, y1, t1 = c1
-    x2, y2, t2 = c2
-    if (x1, y1) != (x2, y2):
-        distance += 1
-    if t1 != t2:
-        distance += 7
-    return distance
-
-
-def dijkstra(start, target, cave_map):
-    """
-    Init priority queue with start node at 0 and all others at Infinity
-    PQ keeps track of where we've been by storing the previous node and the shortest distance
-    1) For each neighbour of node with closest distance
-    2) If distance to next node is smaller than current distance in PQ, replace
-        with distance and this node as previous node
-    Every time all neighbours for the node under consideration have been checked,
-    remove the node from the PQ
-
-    """
-    large_int = 1 << 64
-    pq = []  # {node: (distance_from_source, previous_node)}
+    pq = [(0, 0, 0, TORCH)]  # distance, x, y, tool
     push = heapq.heappush
     pop = heapq.heappop
-    push(pq, (0, start))
-    shortest_paths = {}  # keep track of final shortest paths
-    seen = {}
-    while pq:
-        distance, candidate = pop(pq)
-        if candidate in shortest_paths:
+    visited = set()
+    target = (tx, ty, TORCH)
+    p1 = sum([v for k, v in grid.items() if k[0] <= tx and k[1] <= ty])
+    shortest = {}
+    large_int = 1 << 64
+
+    while True:
+        d, x, y, tool = pop(pq)
+        if (x, y, tool) in visited:
             continue
 
-        # store final value for candidate vertex
-        shortest_paths[candidate] = distance
+        if (x, y, tool) == target:
+            return p1, d
 
-        for neighbour in list(neighbours(
-                cave_map,
-                *candidate,
-                visited=set(shortest_paths)
-            )):
-            uv_distance = distance + _distance(neighbour, candidate)
-            if uv_distance <= seen.get(neighbour, large_int):
-                seen[neighbour] = uv_distance
-                push(pq, (uv_distance, neighbour))
-    return shortest_paths[target]
+        visited.add((x, y, tool))
+        shortest[(x, y, tool)] = d
+        neighbours = set([])
+        for nx, ny in [
+            (x - 1, y),
+            (x + 1, y),
+            (x, y - 1),
+            (x, y + 1),
+        ]:
+            if (nx, ny) not in grid:
+                continue
 
+            if tool in tools_for[grid[(nx, ny)]] and (nx, ny, tool) not in visited:
+                neighbours.add((d + 1, nx, ny, tool))
 
-def solve2(depth, tx, ty):
-    cave_map = generate_cave_map(depth, tx + 300, ty + 300)
-    return dijkstra((0, 0, TORCH), (tx, ty, TORCH), cave_map)
+        for t in (tools_for[grid[(x, y)]] - set([tool])):
+            if (x, y, t) not in visited:
+                neighbours.add((d + 7, x, y, t))
+
+        for d, nx, ny, tool in neighbours:
+            if d < shortest.get((nx, ny, tool), large_int):
+                shortest[(nx, ny, tool)] = d
+            push(pq, (d, nx, ny, tool))
+
+    raise Exception("never hit target")
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     depth, tx, ty = args.depth, args.targetX, args.targetY
-    print("Part 1: " + str(solve(depth, tx, ty)))
-    print("Part 2: " + str(solve2(depth, tx, ty)))
+    print("Part 1: {:d}\nPart 2: {:d}".format(*solve(depth, tx, ty)))
