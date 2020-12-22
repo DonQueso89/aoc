e44206f88d77c1df8ae8ec3d01804919fa8e3442
DonQueso89 <kg.v.ekeren@gmail.com>
2020-12-22 14:52:29 +0100

    d22

--- 2020/day19/solve.jl
+++ 2020/day19/solve.jl
@@ -24,9 +24,9 @@ end
 
 max_length = maximum(length, messages)
 
+memo = Dict{Int, Any}()
 function lookup(n:: Int16, depth=1)
     conds_or_scalar = rules[n]
-    memo = Dict{Int, Any}()
     if conds_or_scalar in ('a', 'b')
         return [conds_or_scalar]
     end
@@ -35,11 +35,55 @@ function lookup(n:: Int16, depth=1)
 
     for cond in conds_or_scalar
         alts = []
-        alts = [get!(function() lookup(number, depth+1) end, memo, number) for number in cond]
+        alts = [get!(function() lookup(number,depth+1) end, memo, number) for number in cond]
         push!(alternatives, filter(x -> length(x) <= max_length, map(join, Iterators.product(alts...)))...)
     end
-
     return alternatives
 
 end
-@time println("1: $(length(intersect(lookup(Int16(0)), messages)))")
+
+patterns = lookup(Int16(0))
+valid = intersect(patterns, messages)
+println("1: $(length(valid))")
+
+rest = setdiff(messages, patterns)
+"""
+8: 42 | 42 8
+11: 42 31 | 42 11 31
+"""
+rgx1 = Regex(join(map(x -> "^"*x, memo[42]), "|"))
+rgx2 = Regex(join(map(x -> "^"*x, memo[31]), "|"))
+
+patts = Dict(42=>rgx1, 31=>rgx2)
+
+function match_layered(s::AbstractString)
+    trimmed = s
+    path = []
+    while length(trimmed) > 0
+        if !(match(patts[42], trimmed) === nothing)
+            trimmed = replace(trimmed, patts[42]=>"")
+            push!(path, 42)
+        elseif !(match(patts[31], trimmed) === nothing)
+            trimmed = replace(trimmed, patts[31]=>"")
+            push!(path, 31)
+        else
+            break
+        end
+    end
+
+    if length(trimmed) == 0
+        l = length(path)
+        println(path)
+    end
+
+    return false
+end
+
+n_valid = 0
+for msg in messages
+    if match_layered(msg)
+        global n_valid += 1
+    end
+end
+
+println("2: $(n_valid + length(valid))")
---
+++ 2020/day20/solve.jl
@@ -0,0 +1,97 @@
+import Base.display
+
+
+if length(ARGS) > 0
+    fname = ARGS[1]
+else
+    fname = "input"
+end
+
+inp = split(read(open(fname), String), "\n\n")
+
+mutable struct Tile
+    id::Int
+    g::BitArray{2}
+
+    # fits
+    right:: Array{Tile}
+    left:: Array{Tile}
+    top:: Array{Tile}
+    bottom:: Array{Tile}
+
+    nfits::Int
+    oriented::Bool
+
+    function Tile(id, g)
+        new(id, g, [], [], [], [], 0, false)
+    end
+end
+
+display(t::Tile) = display(t.g)
+borders(t::Tile) = (t.g[:,end], t.g[:,1], t.g[1,:], t.g[end,:]) # right, left, top, bottom
+section(t::Tile) = println("\n     $(length(t.top) > 0 ? t.top[1].id : "XXXX")     \n$(length(t.left) > 0 ? t.left[1].id : "XXXX") $(t.id) $(length(t.right) > 0 ? t.right[1].id : "XXXX")\n     $(length(t.bottom) > 0 ? t.bottom[1].id : "XXXX")     ")
+function tile(s)
+    s = split(s, "\n")
+    id = parse(Int, s[1][6:9])
+
+    layout = s = transpose(hcat([[y == '#' for y in x] for x in s[2:end]]...)) .== 1
+    return Tile(id, layout)
+end
+
+function bootstrap(last_oriented_tile, tiles)
+    # orient 1 pair, then find the rest
+    left_adj, right_adj, top_adj, bottom_adj = nothing, nothing, nothing, nothing
+    for tile in tiles
+        if tile.oriented
+            continue
+        end
+
+        right, left, top, bottom = borders(last_oriented_tile)
+        for flipfn in [reverse, reverse]
+            tile.g = flipfn(tile.g, dims=2)
+            for rotfn in [rotr90, rotr90, rotr90, rotr90]
+                tile.g = rotfn(tile.g)
+                tright, tleft, ttop, tbottom = borders(tile)
+
+                if all(tright .== left)
+                    push!(tile.right, last_oriented_tile)
+                    push!(last_oriented_tile.left, tile)
+                    tile.oriented = true
+                    left_adj = tile
+                    break
+                elseif all(tleft .== right)
+                    push!(tile.left, last_oriented_tile)
+                    push!(last_oriented_tile.right, tile)
+                    tile.oriented = true
+                    right_adj = tile
+                    break
+                elseif all(ttop .== bottom)
+                    push!(tile.top, last_oriented_tile)
+                    push!(last_oriented_tile.bottom, tile)
+                    tile.oriented = true
+                    bottom_adj = tile
+                    break
+                elseif all(tbottom .== top)
+                    push!(tile.bottom, last_oriented_tile)
+                    push!(last_oriented_tile.top, tile)
+                    tile.oriented = true
+                    top_adj = tile
+                    break
+                end
+            end
+        end
+    end
+end
+
+
+tiles = map(tile, inp)
+
+tiles[1].oriented = true
+for tile in tiles
+    bootstrap(tile, tiles)
+end
+
+
+for tile in tiles
+    section(tile)
+end
