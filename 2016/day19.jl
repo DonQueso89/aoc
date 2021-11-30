b49dab0ceb996f44cfaa9ca9ac65f44f1cd11eee
DonQueso89 <kg.v.ekeren@gmail.com>
2021-11-30 22:10:25 +0100

    solve 2016 d19 pt2

--- 2016/day19.jl
+++ 2016/day19.jl
@@ -1,4 +1,5 @@
-# possible alternative: something with subtracting consecutive geometric series
+# possible alternative: something with subtracting consecutive arithmetic series
+S(a0, an, s) = (an^2 - a0^2 + s*(an+a0)) / 2s # arbitrary arithmetic series
 function solve(len, first, last, step)
     if len == 1
         return first
@@ -13,5 +14,44 @@ n = 3012210
 
 println(solve(n, 1, n, 1))
 
-# start at (n - (n % 2)) / 2
-# pineut pattern = skip 1, skip 0, skip 1, ....
+
+# Second solution is only completed for even n (laziness)
+mutable struct Node
+  v:: Int64
+  nxt:: Node
+  Node(v::Int64) = (tmp = new(v); tmp.nxt = tmp)
+end
+
+function circle(n)
+           root = Node(n)
+           tmp = root
+           initial = tmp
+           for i in (n-1):-1:1
+               tmp_ = Node(i)
+               tmp_.nxt = tmp
+               tmp = tmp_
+               if i == n / 2
+                   initial = tmp
+               end
+           end
+           root.nxt = tmp
+           return initial
+       end
+
+function solve2(n)
+  voor_pineut = circle(n)
+  even = true
+  while voor_pineut.nxt != voor_pineut
+    pineut = voor_pineut.nxt
+    #println("Eliminating ", pineut.v)
+    voor_pineut.nxt = pineut.nxt
+    even = !even
+
+    if even
+      voor_pineut = pineut.nxt
+    end
+  end
+  return voor_pineut
+end
+
+println(solve2(n))
