a8b485859ee160dafaec0ac34f74034b87c35a76
DonQueso89 <kg.v.ekeren@gmail.com>
2021-12-15 00:05:04 +0100

    14

--- 2021/14/solve.jl
+++ 2021/14/solve.jl
@@ -4,54 +4,72 @@ inp = open(infl)
 polymer = readline(inp)
 
 t = Dict(map(eval ∘ Meta.parse, split(replace(replace(read(inp, String), "->" => "=>"), r"([A-Z]+)" => s"\"\1\""), "\n")))
-NULL = 'n'
 N = 40
 
-mutable struct E
-    left::Union{Char, E}
-    right::Union{Char, E}
-    v::Char
-end
-println(polymer)
-println(t)
 function solve(polymer, t)
-    d = Dict{Char, Int}()
+    pairs_ = Dict{String, Int}()
+    first_, last_ = polymer[1], polymer[end]
 
-    entry = E(E(NULL, NULL, polymer[1]), NULL, polymer[2])
-    prev = entry
-    for c in polymer[3:end]
-        cur = E(prev, NULL, c)
-        prev.right = cur
-        prev = cur
+    for i in 2:length(polymer)
+        p = String(polymer[i-1:i])
+        pairs_[p] = get(pairs_, p, 0) + 1
     end
-
-    for c in polymer
-        d[c] = get(d, c, 0) + 1
-    end
-
-    n = 0
-    while n < 20
-        cur = entry
-        entry = nothing
-        while cur != NULL
-            s = String([cur.left.v, cur.v])
-            repl = t[s][1]
-            insert_ = E(cur.left, cur, repl)
-            cur.left.right = insert_
-            cur.left = insert_
-            if isnothing(entry)
-                entry = insert_
-            end
-            d[repl] = get(d, repl, 0) + 1
-            cur = cur.right
+    it_ = 0
+    while it_ < N
+        nxt = Dict{String, Int}()
+        println(it_)
+        keys_ = keys(pairs_)
+        for k in keys_
+            println(k)
+            a, b = String([k[1], t[k][1]]), String([t[k][1], k[2]])
+            nxt[a] = get(nxt, a, 0) + pairs_[k]
+            nxt[b] = get(nxt, b, 0) + pairs_[k]
         end
-        n+=1
+        pairs_ = nxt
+        it_ += 1
+    end
+    cnt:: Dict{Char, Float64} = Dict()
+    for k in keys(pairs_)
+        for c in k
+            cnt[c] = get(cnt, c, 0) + pairs_[k]
+        end
     end
 
-   maximum(values(d)) - minimum(values(d)), sum(values(d))
+    for k in keys(cnt)
+        cnt[k] /= 2
+    end
+    cnt[first_] += .5
+    cnt[last_] += .5
+
+
+    maximum(values(cnt)) - minimum(values(cnt))
 
 end
 
 println(solve(polymer, t))
 
-e = E(NULL, NULL, NULL)
+"""
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+
+NNCB (NN NC CB) -> NC CN NB BC CH HB
+NCNBCHB -> NB BC CC CN NB BB BB BC CB BH HC CB
+NBCCNBBBCBHCB -> NB BB BB BC CN NC  
+NBBBCNCCNBBNBNBBCHBHHBCHB  NB BB 
+
+"""
